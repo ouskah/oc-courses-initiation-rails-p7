f6995dbde3b18dd53280e8d6bf92d36f7ca2ccf5
ouskah <ouskah.gn@gmail.com>
2016-01-23 23:00:35 +0100

    generation d'un scaffold book

---
+++ app/views/books/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @book, :id, :title, :content, :author_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123214031) do
+ActiveRecord::Schema.define(version: 20160123215808) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20160123214031) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "books", ["author_id"], name: "index_books_on_author_id"
 
 end
